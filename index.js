662659f3af7bb5bc1de74f8ed8ca3f449dc6b3f4
sayed udoy <umudoy364@gmail.com>
2024-10-23 12:13:53 +0500

    added delete functionality to delete existing to-do items

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const todoInput = document.getElementById("todoInput");
 const todoList = document.getElementById("todoList");
 const addBtn = document.getElementById("addBtn");
 
+let isEdit = null;
+
 function createElement(el, text, classNm) {
   let element = document.createElement(el);
   if (text !== undefined && text !== "") {
@@ -13,23 +15,36 @@ function createElement(el, text, classNm) {
   return element;
 }
 
-function bindingChildWithParent(parent,child){
-  parent.appendChild(child)
+function bindingChildWithParent(parent, child) {
+  parent.appendChild(child);
 }
 
 function addTodoList() {
   let inputText = todoInput.value;
-  const li = createElement("li", "", "todoItem");
-  let p = createElement("p", inputText, "flex-1");
-  bindingChildWithParent(li,p)
-  const editBtn = createElement("button", "Edit", "editBtn");
-  bindingChildWithParent(li,editBtn)
-  const deleteBtn = createElement("button", "Delete", "deleteBtn");
-  bindingChildWithParent(li,deleteBtn)
-  // binding li with todo list
-  bindingChildWithParent(todoList,li)
-  todoInput.value = "";
-  todoInput.focus();
+  if (inputText.length <= 0) {
+    alert("input is empty");
+    return false;
+  } else {
+    const li = createElement("li", "", "todoItem");
+    let p = createElement("p", inputText, "flex-1");
+    bindingChildWithParent(li, p);
+    const editBtn = createElement("button", "Edit", "editBtn");
+    bindingChildWithParent(li, editBtn);
+    const deleteBtn = createElement("button", "Delete", "deleteBtn");
+    bindingChildWithParent(li, deleteBtn);
+    // binding li with todo list
+    bindingChildWithParent(todoList, li);
+    todoInput.value = "";
+    todoInput.focus();
+  }
+}
+
+function editTodoList(e) {
+  const event = e.target;
+  if (event.innerHTML === "Delete") {
+    event.parentNode.remove();
+  }
 }
 
 addBtn.addEventListener("click", addTodoList);
+todoList.addEventListener("click", editTodoList);
