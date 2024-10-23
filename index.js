64cfa951fee93d074620d35e4737331cd4893ef4
sayed udoy <umudoy364@gmail.com>
2024-10-23 11:52:04 +0500

    i have added add function to add todo to the todo list and changed some style of the ui

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const todoInput = document.getElementById("todoInput");
 const todoList = document.getElementById("todoList");
 const addBtn = document.getElementById("addBtn");
 
-function createElement(el,text,classNm){
+function createElement(el, text, classNm) {
   let element = document.createElement(el);
   if (text !== undefined && text !== "") {
     element.innerHTML = text;
@@ -12,3 +12,20 @@ function createElement(el,text,classNm){
   }
   return element;
 }
+
+function addTodoList() {
+  let inputText = todoInput.value;
+  const li = createElement("li", "", "todoItem");
+  let p = createElement("p", inputText, "flex-1");
+  li.appendChild(p);
+  const editBtn = createElement("button", "Edit", "editBtn");
+  li.appendChild(editBtn);
+  const deleteBtn = createElement("button", "Delete", "deleteBtn");
+  li.appendChild(deleteBtn);
+  // binding li with todo list
+  todoList.appendChild(li);
+  todoInput.value = "";
+  todoInput.focus();
+}
+
+addBtn.addEventListener("click", addTodoList);
