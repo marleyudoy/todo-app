254cdbe51f5750ee52832e6165512cd7189c1de5
sayed udoy <umudoy364@gmail.com>
2024-10-23 11:16:30 +0500

    i have created a createElement Function. which is creates element, that i want

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const todoInput = document.getElementById("todoInput");
 const todoList = document.getElementById("todoList");
 const addBtn = document.getElementById("addBtn");
+
+function createElement(el,text,classNm){
+  let element = document.createElement(el);
+  if (text !== undefined && text !== "") {
+    element.innerHTML = text;
+  }
+  if (classNm !== undefined && classNm !== "") {
+    element.classList.add(classNm);
+  }
+  return element;
+}
